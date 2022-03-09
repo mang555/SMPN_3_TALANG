7cddb40aff8a94df2e36a2430a2bfaf5797b3529
mang555 <akmal.fahrian346@gmail.com>
2022-03-09 23:13:35 +0700

    nw

--- app/src/main/java/com/example/smpn3talang/Eskul.java
+++ app/src/main/java/com/example/smpn3talang/Eskul.java
@@ -2,9 +2,12 @@ package com.example.smpn3talang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 public class Eskul extends AppCompatActivity {
@@ -18,20 +21,38 @@ public class Eskul extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eskul);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 
     public void osis(View view) {
+        Intent intent = new Intent(Eskul.this, OsisActivity.class);
+        startActivity(intent);
     }
 
     public void pramuka(View view) {
+        Intent intent = new Intent(Eskul.this, PramukaActivity.class);
+        startActivity(intent);
     }
 
     public void futsal(View view) {
+        Intent intent = new Intent(Eskul.this, FutsalActivity.class);
+        startActivity(intent);
     }
 
     public void pmr(View view) {
+        Intent intent = new Intent(Eskul.this, PmrActivity.class);
+        startActivity(intent);
     }
 
     public void bulutangkis(View view) {
+        Intent intent = new Intent(Eskul.this, VoliActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/smpn3talang/FutsalActivity.java
@@ -0,0 +1,14 @@
+package com.example.smpn3talang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FutsalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_futsal);
+    }
+}
