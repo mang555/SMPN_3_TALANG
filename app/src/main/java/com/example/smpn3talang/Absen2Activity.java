05925e6ffe4b409a164c761ce5b5117d466c6435
mang555 <akmal.fahrian346@gmail.com>
2022-03-24 13:22:22 +0700

    asdfdgfhG

---
+++ app/src/main/java/com/example/smpn3talang/Absen2Activity.java
@@ -0,0 +1,14 @@
+package com.example.smpn3talang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Absen2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absen2);
+    }
+}
--- app/src/main/java/com/example/smpn3talang/AbsenActivity.java
+++ app/src/main/java/com/example/smpn3talang/AbsenActivity.java
@@ -2,6 +2,7 @@ package com.example.smpn3talang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -14,18 +15,16 @@ public class AbsenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absen);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
 
-//        tiga baris dibawah agar laman dapat di zoom
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setBuiltInZoomControls(false);
+    }
 
-//        untuk menambah scrollbar
-        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://presensi-smpn3talang.epizy.com/");
+    public void admin(View view) {
+        Intent intent = new Intent(AbsenActivity.this, Absen2Activity.class);
+        startActivity(intent);
+    }
+
+    public void absen(View view) {
+        Intent intent = new Intent(AbsenActivity.this, AdminAbsenActivity.class);
+        startActivity(intent);
     }
 }
