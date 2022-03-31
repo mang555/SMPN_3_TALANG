247a84041f1445280e7c786e05d7ac6ab106497e
mang555 <akmal.fahrian346@gmail.com>
2022-03-31 13:00:17 +0700

    asdfdgfh

--- app/src/main/java/com/example/smpn3talang/Eskul.java
+++ app/src/main/java/com/example/smpn3talang/Eskul.java
@@ -6,31 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
-import com.smarteist.autoimageslider.SliderView;
 
 public class Eskul extends AppCompatActivity {
-    SliderView sliderView;
-    int[] images = {R.drawable.sldrvw,
-            R.drawable.sldrvw1,
-            R.drawable.viewsld,
-            R.drawable.viewsld1
 
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eskul);
-        sliderView = findViewById(R.id.image_slider);
 
-        SliderAdapter sliderAdapter = new SliderAdapter(images);
-
-        sliderView.setSliderAdapter(sliderAdapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
     }
 
 
