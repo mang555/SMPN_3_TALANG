4c5a72cc2d76d0acae72b71235677f11911f86fe
mang555 <akmal.fahrian346@gmail.com>
2022-04-16 22:50:23 +0700

    nww1vgmnnj

--- app/src/main/java/com/example/smpn3talang/PramukaActivity.java
+++ app/src/main/java/com/example/smpn3talang/PramukaActivity.java
@@ -2,7 +2,9 @@ package com.example.smpn3talang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PramukaActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PramukaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pramuka);
+    }
+
+    public void album8(View view) {
+        Intent intent = new Intent(PramukaActivity.this, album8Activity.class);
+        startActivity(intent);
     }
 }
