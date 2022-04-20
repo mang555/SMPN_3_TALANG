06b97fd9f48981a4969a60439625365932358dae
mang555 <akmal.fahrian346@gmail.com>
2022-04-20 23:16:14 +0700

    nww1vgmnndBnn

--- app/src/main/java/com/example/smpn3talang/SplashScreenActivity.java
+++ app/src/main/java/com/example/smpn3talang/SplashScreenActivity.java
@@ -3,9 +3,14 @@ package com.example.smpn3talang;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
@@ -13,14 +18,27 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                    Toast.makeText(SplashScreenActivity.this,"LOGIN SUKSES COY",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(SplashScreenActivity.this,"SALAH COY!!",Toast.LENGTH_SHORT).show();
             }
-        },  3000L);//3 detik
+        });
 
     }
 }
